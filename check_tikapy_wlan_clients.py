7aed10a74c6d80b0bfb16d9ae3057e0596c95bbf
Andre Keller <ak@ak.cx>
2015-06-16 00:24:00 +0200

    Feature: Add new script to check clients connected to a specific WLAN.
    This adds a simple check script to report clients connected to a
    specific WLAN (based on its SSID). The scripts will also report
    the count of the connected clients as performance data.

---
+++ check_tikapy_wlan_clients.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+nagios/icinga plugin to check connected clients of a wlan
+'''
+
+__author__ = 'Andre Keller <andre.keller@vshn.ch>'
+__copyright__ = 'Copyright (c) 2015, VSHN AG, andre.keller@vshn.ch'
+__license__ = 'BSD'
+__version__ = '0.1.0'
